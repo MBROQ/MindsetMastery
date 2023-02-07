93864d7d5b62720780ab63cfaf614d0e7d3edb90
MBROQ <mark.berryman@roq.co.uk>
2023-02-07 15:46:33 +0000

    FIrstAttempt

--- fibonacci.test.js
+++ fibonacci.test.js
@@ -7,6 +7,42 @@ beforeEach(() => {
 });
 
 test('When initialized the next number can be returned', () => {
-    fibonacci.init(8);
-    expect(fibonacci.next()).toBe(13);
+    fibonacci.init(2);
+    expect(fibonacci.next()).toBe(3);
 });
+
+test('When initialized the next number can be returned', () => {
+    fibonacci.init(13);
+    expect(fibonacci.next()).toBe(21);
+
+})
+
+test('When initialized the next number can be returned', () => {
+    fibonacci.init(0);
+    expect(fibonacci.next()).toBe(1);
+
+})
+
+test('When initialized skip forward', () => {
+    fibonacci.init(1);
+    expect(fibonacci.skip()).toBe(2);
+
+})
+
+test('When initialized skip forward', () => {
+    fibonacci.init(3);
+    expect(fibonacci.next(8)).toBe(5);
+
+})
+
+test('When initialized skip forward', () => {
+    fibonacci.init(3);
+    expect(fibonacci.skip(8)).toBe(233);
+
+})
+
+test('When initialized skip forward', () => {
+    fibonacci.init();
+    expect(fibonacci.next()).toBe(1);
+
+})
