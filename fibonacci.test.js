101866737b21db11debe3e4286f45a808cc32f86
MBROQ <mark.berryman@roq.co.uk>
2023-02-08 09:23:52 +0000

    TestsComplete

--- fibonacci.test.js
+++ fibonacci.test.js
@@ -6,43 +6,49 @@ beforeEach(() => {
     fibonacci = new Fibonacci();
 });
 
-test('When initialized the next number can be returned', () => {
+test('When initialized "2" the next number to show', () => {
     fibonacci.init(2);
     expect(fibonacci.next()).toBe(3);
 });
 
-test('When initialized the next number can be returned', () => {
+test('When initialized "13" the next number to show ', () => {
     fibonacci.init(13);
     expect(fibonacci.next()).toBe(21);
 
 })
 
-test('When initialized the next number can be returned', () => {
+test('When initialized "0" the next number to show', () => {
     fibonacci.init(0);
     expect(fibonacci.next()).toBe(1);
 
 })
 
-test('When initialized skip forward', () => {
+test('When initialized "1" skip forward', () => {
     fibonacci.init(1);
     expect(fibonacci.skip()).toBe(2);
 
 })
 
-test('When initialized skip forward', () => {
+test('Initialized at "3" Condition added to next function', () => {
     fibonacci.init(3);
     expect(fibonacci.next(8)).toBe(5);
 
 })
 
-test('When initialized skip forward', () => {
+test('Initialized at "3" skip "8"', () => {
     fibonacci.init(3);
     expect(fibonacci.skip(8)).toBe(233);
 
 })
 
-test('When initialized skip forward', () => {
+test('No function in "init"', () => {
     fibonacci.init();
     expect(fibonacci.next()).toBe(1);
 
 })
+
+test('When Initialized "21" next number to show with next condition "8"', () => {
+    fibonacci.init(21);
+    expect(fibonacci.next(8)).toBe(34);
+
+});
